2520bf669104acce7b4d9319477a78c72dabc54e
Gabriel Hephzibah <gabrielhephzibah24@gmail.com>
2021-03-28 08:27:52 +0100

    my_first_commit

---
+++ app/src/main/java/courses/pluralsight/com/tabianconsulting/models/fcm/FirebaseCloudMessage.java
@@ -0,0 +1,42 @@
+package courses.pluralsight.com.tabianconsulting.models.fcm;
+
+
+public class FirebaseCloudMessage {
+
+    private String to;
+    private Data data;
+
+    public FirebaseCloudMessage(String to, Data data) {
+        this.to = to;
+        this.data = data;
+    }
+
+    public FirebaseCloudMessage() {
+
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+
+    public Data getData() {
+        return data;
+    }
+
+    public void setData(Data data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "FirebaseCloudMessage{" +
+                "to='" + to + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
